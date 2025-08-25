e7f992405d221d31bc94525587727a78d127cfb3
bilelsalemdev <bilelsalemdev@gmail.com>
2025-08-25 10:45:24 +0100

    fix: fix hostname for imgaes

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.builder.io",
+        port: "",
+        pathname: "/api/v1/image/**",
+      },
     ],
   },
 };
